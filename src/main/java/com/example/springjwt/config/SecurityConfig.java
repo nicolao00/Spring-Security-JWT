67286e6d9843e7222f61040349d4c5c2164ee778
nicolao00 <nicolao00@naver.com>
2024-09-16 15:14:05 +0900

    feat: JWT 검증 필터 구현

--- src/main/java/com/example/springjwt/config/SecurityConfig.java
+++ src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springjwt.config;
 
+import com.example.springjwt.jwt.JWTFilter;
 import com.example.springjwt.jwt.JWTUtil;
 import com.example.springjwt.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
@@ -56,6 +57,10 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
+
+        //JWTFilter 등록 (로그인 필터 작동 전에 동작하도록 위치 설정)
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
 
         //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 빈 등록 필요
         // 기존 시큐리티 필터에 있는 UsernamePasswordAuthenticationFilter에 우리가 작성한 새로운 필터를 덮어 씌울거라 addFilterAt을 사용했음.
--- src/main/java/com/example/springjwt/jwt/JWTFilter.java
+++ src/main/java/com/example/springjwt/jwt/JWTFilter.java
@@ -1,9 +1,14 @@
 package com.example.springjwt.jwt;
 
+import com.example.springjwt.dto.CustomUserDetails;
+import com.example.springjwt.entity.UserEntity;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -19,7 +24,7 @@ public class JWTFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        //request에서 Authorization 헤더를 찾음
+        // request에서 Authorization 헤더를 찾음
         String authorization= request.getHeader("Authorization");
 
         //Authorization 헤더 검증
@@ -41,6 +46,32 @@ public class JWTFilter extends OncePerRequestFilter {
         if (jwtUtil.isExpired(token)) {
 
             System.out.println("token expired");
-            filterChain.doFilter(request,
+            filterChain.doFilter(request, response);
+
+            //조건이 해당되면 메소드 종료 (필수)
+            return;
+        }
+
+        //토큰에서 username과 role 획득
+        String username = jwtUtil.getUsername(token);
+        String role = jwtUtil.getRole(token);
+
+        //userEntity를 생성하여 값 set
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(username);
+        // 임시 비밀번호("temppassword")를 설정한 이유는, 비밀번호는 UserDetails 인터페이스의 메소드(getPassword()) 구현을 위해 필요하지만, JWT 인증에서는 실제 비밀번호가 필요하지 않기 때문!!!!!!!
+        // JWT 토큰은 이미 인증된 사용자를 식별하는 데 필요한 정보를 포함하고 있으며, JWT의 유효성만 확인되면 사용자를 인증된 것으로 간주합니다.
+        userEntity.setPassword("temppassword");
+        userEntity.setRole(role);
+
+        //UserDetails에 회원 정보 객체 담기
+        CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
+
+        //스프링 시큐리티 인증 토큰 생성
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+        //세션에 사용자 등록
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
     }
 }
